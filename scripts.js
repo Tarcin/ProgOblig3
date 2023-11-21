e492fd28599fc447a7730d3dd45eb6d690d60d12
Tarcin <axelgh@student.matnat.uio.no>
2023-11-21 11:20:52 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-
+function circle(radius, fillmode, color){
+const image = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+image.setAttribute("rx", radius);
+image.setAttribute("cy", radius);
+image.setAttribute("r", radius);
+//circle.setAttribute("stroke", fillmode);
+image.setAttribute("fill", color);
+return image;
+}
+function overlayXY(img2, x, y, img1) {
+//img2 is overlaid on top of img1
+img2.appendChild(img1)
+}
