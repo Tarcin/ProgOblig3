8c6d08c74814ffc3e51cab8fb06ab74af85e1347
Tarcin <axelgh@student.matnat.uio.no>
2023-11-21 19:27:30 +0100

    Update figures.js

--- js/figures.js
+++ js/figures.js
@@ -10,9 +10,9 @@ return image;
 }
 
 function rectangleSVG(width, height, fillmode, color){
-var image = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+var image = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 image.setAttribute("width", width);
-image.setAttribute("length", length);
+image.setAttribute("height", height);
 if(fillmode == "outline"){
     image.setAttribute("stroke", color);
 } else if(fillmode == "solid") {
@@ -20,9 +20,15 @@ if(fillmode == "outline"){
 }
 return image;
 }
-    function overlayXYsvg(img2, x, y, img1) {
+  
+function overlayXYsvg(img2, x, y, img1) {
 //img2 is overlaid on top of img1 at coordinates
 img2.appendChild(img1)
-img1.setAttribute("cx", x);
-img1.setAttribute("cy", y);
+  if(img1.tagName == "circle"){
+    img1.setAttribute("cx", x);
+    img1.setAttribute("cy", y);
+  } else if(img1.tagName == "rect"){
+    img1.setAttribute("x", x);
+    img1.setAttribute("y", y);
+  }
 }
