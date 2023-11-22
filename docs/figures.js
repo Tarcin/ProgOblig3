0e7e1b2ee9ee83329780effcbd7563970a9ece8c
Tarcin <axelgh@student.matnat.uio.no>
2023-11-22 14:41:55 +0100

    Update figures.js

--- docs/figures.js
+++ docs/figures.js
@@ -35,17 +35,68 @@ img2.appendChild(img1)
 }
 
 //CANVAS FUNCTIONS
-  function circle(radius, fillmode, color){
-    var canvas = document.getElementById("myCanvas");
+function circle(radius, fillmode, color){
+    var canvas = document.createElement("canvas");
+    canvas.width = (radius*2)+5;
+    canvas.height = (radius*2)+5;
     var ctx = canvas.getContext("2d");
-    var circX = canvas.width / 2;
-    var circY = canvas.height / 2;
     ctx.beginPath();
-    ctx.arc(circX, circY, radius, 0, 2 * Math.PI);
+    ctx.arc(radius+2, radius+2, radius, 0, 2 * Math.PI);
     if (fillmode == "solid"){
         ctx.fillStyle = color;
         ctx.fill();
-    } else if (fillMode == "outline"){
+    } else if (fillmode == "outline"){
+        ctx.strokeStyle = color;
         ctx.stroke();
     }
+    return canvas;
 }
+
+function rectangle(width, height, fillmode, color){
+    var canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    var ctx = canvas.getContext("2d");
+    ctx.rect(0, 0, width, height);
+    if (fillmode == "solid"){
+        ctx.fillStyle = color;
+        ctx.fill();
+    } else if (fillmode == "outline"){
+        ctx.strokeStyle = color;
+        ctx.stroke();
+    }
+    return canvas;
+}
+
+
+function overlayXY(img2, x, y, img1){
+    img2.drawImage(img1, x, y);
+}
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+const centerX = canvas.width/2;
+const centerY = canvas.height/2; 
+var temp = rectangle(60, 60, "solid", "yellow");
+overlayXY(ctx, centerX - (temp.width/2), centerY - (temp.height/2), temp);
+temp = rectangle(50, 50, "solid", "green");
+overlayXY(ctx, centerX - (temp.width/2), centerY - (temp.height/2), temp);
+temp = rectangle(40, 40, "solid", "red");
+overlayXY(ctx, centerX - (temp.width/2), centerY - (temp.height/2), temp);
+temp = rectangle(30, 30, "solid", "blue")
+overlayXY(ctx, centerX - (temp.width/2), centerY - (temp.height/2), temp);
+temp = rectangle(20, 20, "solid", "black")
+overlayXY(ctx, centerX - (temp.width/2), centerY - (temp.height/2), temp);
+temp = circle(100, "outline", "black");
+overlayXY(ctx, centerX - (temp.width/2), centerY - (temp.height/2), temp);
+
+
+
+/*var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+overlayXY(ctx, canvas.height / 2, canvas.height / 2, circle(30, "solid", "black"));
+overlayXY(ctx, (canvas.height / 2) - 40, (canvas.height / 2) - 40, circle(30, "solid", "green"));
+overlayXY(ctx, (canvas.height / 2) + 40, (canvas.height / 2) + 40, circle(30, "solid", "red"));
+overlayXY(ctx, (canvas.height / 2) - 40, (canvas.height / 2) + 40, circle(30, "solid", "blue"));
+overlayXY(ctx, (canvas.height / 2) + 40, (canvas.height / 2) - 40, circle(30, "solid", "yellow"));*/
